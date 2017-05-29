9412e06192b730cd973b507f1cc8dcd31c6ce5b9
VBhat97 <vaibhav.bhat2097@gmail.com>
2017-05-30 01:39:41 +0530

    Add files via upload

---
+++ website/music/views.py
@@ -0,0 +1,15 @@
+from django.http import Http404
+from .models import Album
+from django.shortcuts import render
+
+def index(request):
+    all_albums = Album.objects.all()
+    context = {'all_albums':all_albums}
+    return render(request,'music/index.html', context)
+
+def detail(request,album_id):
+    try :
+         album = Album.objects.get(pk=album_id)
+    except :
+         raise Http404("Album does not exist")
+    return render(request,'music/detail.html',{'album':album})
